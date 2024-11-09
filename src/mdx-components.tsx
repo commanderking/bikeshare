644f6316a4bde4e2a2f97ce29a839b3df15b6035
Jeffrey King <jking@hey.com>
2024-11-09 12:49:01 -0500

    switch to default typography

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,24 +1,5 @@
 import type { MDXComponents } from 'mdx/types'
-import {
-  Header,
-  Code,
-  Paragraph,
-  Link,
-  UnorderedList,
-  Pre,
-} from '@/app/mdx/components'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    h1: ({ children }) => <Header size="6xl">{children}</Header>,
-    h2: ({ children }) => <Header size="4xl">{children}</Header>,
-    h3: ({ children }) => <Header size="2xl">{children}</Header>,
-    h4: ({ children }) => <Header size="xl">{children}</Header>,
-    code: ({ children }) => <Code>{children}</Code>,
-    pre: ({ children }) => <Pre>{children}</Pre>,
-    p: ({ children }) => <Paragraph>{children}</Paragraph>,
-    a: ({ children, href }) => <Link href={href}>{children}</Link>,
-
-    ul: ({ children }) => <UnorderedList>{children}</UnorderedList>,
-  }
+  return {}
 }
